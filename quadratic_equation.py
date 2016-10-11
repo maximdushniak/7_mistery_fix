c97d4c4a272fe82e45a6f607c65b81b1aa69c04f
maximdushniak <maxim.dushniak@gmail.com>
2016-10-11 17:08:45 +0300

    done

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,8 +3,14 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
+    if discriminant < 0:
+        # root1 = complex(-b/(2*a), -sqrt(-discriminant) / (2 * a))
+        # root2 = complex(-b/(2*a), sqrt(-discriminant) / (2 * a))
+        root1 = None
+        root2 = None
+    else:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant == 0:
         return root1, None
     else:
